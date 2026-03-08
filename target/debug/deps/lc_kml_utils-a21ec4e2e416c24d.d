29012eccda14781289e330a9616425bf6a738d7a
laurent.carrie <laurent.carrie@gmail.com>
2026-03-08 10:45:33 +0100

    Initial commit

---
+++ target/debug/deps/lc_kml_utils-a21ec4e2e416c24d.d
@@ -0,0 +1,5 @@
+/Users/laurent/work/lc-kml-utils/target/debug/deps/lc_kml_utils-a21ec4e2e416c24d.d: src/main.rs
+
+/Users/laurent/work/lc-kml-utils/target/debug/deps/liblc_kml_utils-a21ec4e2e416c24d.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/lc_kml_utils-edc82a61b482c086.d
@@ -0,0 +1,5 @@
+/Users/laurent/work/lc-kml-utils/target/debug/deps/lc_kml_utils-edc82a61b482c086.d: src/main.rs
+
+/Users/laurent/work/lc-kml-utils/target/debug/deps/liblc_kml_utils-edc82a61b482c086.rmeta: src/main.rs
+
+src/main.rs:
